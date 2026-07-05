ad9b947af91dadcd151a85c00b5a19acd567434f
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-07-05 14:37:09 +0300

    dev: New project

---
+++ WalnutDrop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WalnutDrop
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
